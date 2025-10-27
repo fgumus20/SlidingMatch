e63d316c1f94c0b06d0174c579e6c62a403bf60f
fgumus20 <fgumus20@ku.edu.tr>
2025-10-27 17:16:35 +0300

    added level selection button

--- Assets/Scripts/Managers/MainMenuManager.cs
+++ Assets/Scripts/Managers/MainMenuManager.cs
@@ -1,8 +1,10 @@
+using DG.Tweening;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenuManager : MonoBehaviour
 {
@@ -12,6 +14,8 @@ public class MainMenuManager : MonoBehaviour
     public GameObject levelsPanel;
     public GameObject levelButtonPrefab;
     public Transform levelsContainer;
+    public GameObject playButton;
+    public GameObject levelsButton;
     void Awake()
     {
         if (instance == null)
@@ -44,15 +48,51 @@ public class MainMenuManager : MonoBehaviour
         SceneManager.LoadScene(i);
     }
 
-
     public void OpenLevelsPanel()
     {
         levelsPanel.SetActive(true);
-    }
+        playButton.SetActive(false);
+        levelsButton.SetActive(false);
+        PopulateLevelButtons();
 
+        var rect = levelsPanel.GetComponent<RectTransform>();
+        rect.localScale = Vector3.one * 0.8f;
+        rect.DOScale(1f, 0.25f).SetEase(Ease.OutBack);
+    }
     public void CloseLevelsPanel()
     {
-        levelsPanel.SetActive(false);
+        var rect = levelsPanel.GetComponent<RectTransform>();
+        rect.DOScale(0.8f, 0.2f).SetEase(Ease.InBack).OnComplete(() => { 
+
+            levelsPanel.SetActive(false);
+            playButton.SetActive(true);
+            levelsButton.SetActive(true);
+        }); 
+
     }
+    public void LoadLevel(int levelIndex)
+    {
+        LevelManager.instance.SetCurrentLevel(levelIndex);
+        LevelManager.instance.SaveLevelProgress();
+        SceneManager.LoadScene(1);
+    }
+
+    public void PopulateLevelButtons()
+    {
+        foreach (Transform child in levelsContainer)
+        {
+            Destroy(child.gameObject);
+        }
+
+        for (int i = 1; i <= 10; i++)
+        {
+            GameObject btn = Instantiate(levelButtonPrefab, levelsContainer);
+            btn.GetComponentInChildren<TextMeshProUGUI>().text = i.ToString();
+
+            int levelIndex = i;
+            btn.GetComponent<Button>().onClick.AddListener(() => LoadLevel(levelIndex));
+        }
+    }
+
 
 }
